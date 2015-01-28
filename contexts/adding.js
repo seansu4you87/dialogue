4f32d7e8685268d9e32f61a57ea0e406aafa193f
Square <git@squareup.com>
2015-01-27 18:17:50 -0800

    Adding context with example spec

---
+++ contexts/adding.js
@@ -0,0 +1,5 @@
+function Adding(a,b) {
+  return a + b;
+}
+
+module.exports = Adding;
---
+++ test/contexts/adding.js
@@ -0,0 +1,16 @@
+var Adding = require('../../contexts/adding');
+var assert = require('assert');
+
+describe(Adding, function() {
+  it('should return the sum of two numbers', function() {
+    assert.equal(Adding(2, 3), 5);
+  });
+
+  it('should be able to accept null numbers', function() {
+    assert.equal(Adding(null, 0), 0);
+  });
+
+  it('should be able to accept undefined numbers', function() {
+    assert.equal(Adding(undefined, 0), NaN);
+  });
+});
