06377557317c5aa69ec6e1cd3929e07383540204
Square <git@squareup.com>
2015-01-27 20:49:50 -0800

    Knex migration for merchant

---
+++ db/knexfile.js
@@ -0,0 +1,55 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      host: "localhost",
+      port: "15432",
+      database: 'myapp',
+      user:     'myapp',
+      password: 'dbpass'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  staging: {
+    client: 'pg',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
---
+++ db/migrations/20150127183640_create_dialogue_database.js
@@ -0,0 +1,16 @@
+'use strict';
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('merchants', function(table) {
+    table.increments().primary();
+    table.uuid('token').unique().notNullable();
+    table.string('email').unique().notNullable();
+    table.string('first_name').notNullable();
+    table.string('last_name').notNullable();
+    table.timestamps();
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('merchants');
+};
